8bc879d233a9693f7ea89c2b2fb399c39954568f
Hamid <hamid@gmail.com>
2024-01-18 23:00:02 +0500

    location

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,51 +1,56 @@
 import React from 'react'
-import data from './Data'
-import { Link, useNavigate } from 'react-router-dom'
+import {data,students} from './Data'
+import { useNavigate } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Banner = () => {
     const navigate = useNavigate();
 
-    const handleClick = ((data)=>{
-      console.log(data,"row---")
-      // localStorage.setItem("blog-data",JSON.stringify(data))
-      if(data){
-        navigate(`/BlogDetail/${data.id}`, { state: data });
+    const handleClick = (data, studentData) => {
+      console.log(data, studentData, "row---");
+      if (data && studentData) {
+        navigate(`/BlogDetail/${data.id}`, { state: data, state1: studentData });
       }
-    })
+    };
+    
   return (
     <div>
-         <>
-      <div className="container  d-flex mt-5">
-        <div className="col-md-6 left ">
-            <h2 className="fs-1 fw-bold my-xl-5 ">Articles for <br /><span className="text-success "> front-end devs</span></h2>
-            <h3 className="cc"> on web performance, <br /> responsive web design and more</h3>
-        </div>
-        <div className="col-md-6 right">
-            <img className="w-100 " src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRGypJJpy6KoVEVGpBXMzTOoz28bilLONjl042Ae-v36YmxQ7ys" alt="" />
+    {data.map((item, index) => (
+      <div className="container" key={index}>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card my-3">
+              <div className="card-body">
+                <h5 className="card-title">{item.title}</h5>
+                <p className="card-text">{item.body}</p>
+                <button onClick={() => handleClick(item, students)}>Read More</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      {data.map((item, index) => (
+    ))}
+    
+    {
+      students.map((items, index) => (
         <div className="container" key={index}>
           <div className="row">
             <div className="col-md-12">
               <div className="card my-3">
                 <div className="card-body">
-                  <h5 className="card-title">{item.title}</h5>
-                  <p className="card-text">{item.body}</p>
-                  {/* Use Link to navigate to BlogDetail */}
-                  {/* <Link to={`/BlogDetail/${index + 1}`} state={item}> Assuming index + 1 as an identifier */}
-                    <button onClick={()=>handleClick(item)}>Read More</button>
-                  {/* </Link> */}
+                  <h5 className="card-title">{items.name}</h5>
+                  <p className="card-text">{items.username}</p>
+                  <button onClick={() => handleClick(items, students)}>BlogDetail</button>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      ))}
-    </>
-    </div>
+      ))
+    }
+   </div> 
   )
+  
 }
 
 export default Banner
--- src/Components/BlogDetail.js
+++ src/Components/BlogDetail.js
@@ -1,32 +1,43 @@
 // BlogDetail.js
 import React from 'react';
-import { useLocation, useParams } from 'react-router-dom';
-import data from './Data';
+import { useLocation } from 'react-router-dom';
 
 const BlogDetail = () => {
-  // const { id } = useParams(); // Retrieve the id parameter from the URL
-const {state} =useLocation()
-console.log(state,"mydata")
-  // Assuming 'data' is an array containing your blog posts
-  // const post = data[parseInt(id, 10) - 1] || {}; // Get the post based on the id
+  const { state, state: state1 } = useLocation(); // Use a single hook and extract both states
+
+  if (!state || !state1) {
+    return <div>No data available</div>; // Handle the case when data is not available
+  }
 
   return (
     <div>
-     <div className="container">
-         <div className="row">
-            <div className="col-md-12">
-              <div className="card my-3">
-                 <div className="card-body">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card my-3">
+              <div className="card-body">
                 <h2 className="card-title">{state.title}</h2>
                 <p className="card-text">{state.body}</p>
                 <p className="card-text">{state.id}</p>
                 <p className="card-text">{state.userId}</p>
-                {/* <p className="card-text">{post.name}</p> */}
-                 </div>
               </div>
             </div>
-         </div>
-         </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card my-3">
+              <div className="card-body">
+                <h2 className="card-title">{state1.name}</h2>
+                <p className="card-text">{state1.username}</p>
+                <p className="card-text">{state1.email}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
